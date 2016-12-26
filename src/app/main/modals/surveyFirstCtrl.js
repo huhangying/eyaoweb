f67ed30640f619f380db943482cfc368394aa6e8
Harry <hhu@live.ca>
2016-12-25 23:52:41 -0500

    implemented surveyFirst page. #2

--- src/app/main/modals/surveyFirstCtrl.js
+++ src/app/main/modals/surveyFirstCtrl.js
@@ -12,8 +12,69 @@
 			var ctrl = this;
 			$scope.surveyFirst = {};
 
+			var checkContent = function() {
+				if (!$scope.groups || $scope.groups.length < 1) {
+					toastr.warning('需要至少一个问卷');
+					return false;
+				}
+
+				for (var i=0; i<$scope.groups.length; i++) {
+					if (!$scope.groups[i] || !$scope.groups[i].surveys || $scope.groups[i].surveys.length < 1) {
+						toastr.warning('每个组内需要至少一个问卷');
+						return false;
+					}
+
+					for (var n=0; n<$scope.groups[i].surveys.length; n++) {
+						if (!$scope.groups[i].surveys[n] || !$scope.groups[i].surveys[n].questions || $scope.groups[i].surveys[n].questions.length < 1) {
+							toastr.warning('每个组每一个问卷至少需要一个问题');
+							return false;
+						}
+
+						for (var m=0; m<$scope.groups[i].surveys[n].questions.length; m++) {
+							var question = $scope.groups[i].surveys[n].questions[m];
+							if (!question.options || question.options.length < 1) {
+								toastr.warning('每个组每一个问卷每一个问题至少需要一个答案');
+								return false;
+							}
+
+							if (question.apply && question.required) { // 问题为必选, 需要监测是否答案已选
+								if (question.answer_type == 3 ) {
+									if (!question.options[0].answer) {
+										toastr.warning('请输入回答问题[' + question.question + ']');
+										return false;
+									}
+								}
+								else {
+									var selectedCount = 0;
+									for (var x=0; x<question.options.length; x++) {
+										if (question.options[x].selected) {
+											selectedCount++;
+										}
+									}
+									if (selectedCount === 0) {
+										toastr.warning('请选择问题[' + question.question + ']的答案');
+										return false;
+									}
+								}
+
+							}
+						}
+					}
+
+				}
+
+				return true;
+			};
+
 			$scope.selectOk = function() {
-				// get article by id
+				// validation first.
+				if (!checkContent()) {
+					return;
+				}
+
+				// save
+
+				// return to main process
 				this.$close($scope.surveyFirst);
 			};
 
@@ -24,7 +85,7 @@
 						return 'radio';
 					case 2:
 						return 'checkbox';
-					case 3:
+					case 3: //text
 						return 'hidden';
 				}
 			};
